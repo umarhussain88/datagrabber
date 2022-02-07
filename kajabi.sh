2dc26259214ea3ce1650c1752a96511507b9b9a0
Umar Hussain <umar.hussain.da@outlook.com>
2022-02-07 02:20:50 +0000

    adding slight change in shell log

--- kajabi.sh
+++ kajabi.sh
@@ -6,4 +6,5 @@ export LC_ALL=en_US.utf-8
 echo "running main.py"
 /usr/local/bin/pipenv run python3 main.py kajabi
 end_datetime=$(date '+%m_%d_%Y_%H_%M_%S')
-echo "${end_datetime} - spider finished successfully"
+echo "${end_datetime} - script finished successfully"
+chm
