5af0035fb8c44a5e3b13fc0ff1b6c4bd8136d70a
Umar Hussain <umar.hussain.da@outlook.com>
2022-02-07 02:12:13 +0000

    adding sh script

---
+++ kajabi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /home/umarh/datagrabber
+start_datetime=$(date '+%m_%d_%Y_%H_%M_%S')
+echo "${start_datetime}"
+export LC_ALL=en_US.utf-8
+echo "running main.py"
+/usr/local/bin/pipenv run python3 main.py kajabi
+end_datetime=$(date '+%m_%d_%Y_%H_%M_%S')
+echo "${end_datetime} - spider finished successfully"
